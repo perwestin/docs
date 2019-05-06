d7bcc7b64f4d24a53f3312caabd9dc13ccf7b5cf
Fredrik Rubensson <fredrik.rubensson@openpayments.io>
2019-05-06 16:07:49 +0200

    problematic max width caused problem only in the static build

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -24,7 +24,6 @@ class Container extends Component {
         <div
           style={{
             margin: '0 auto',
-            maxWidth: 960,
             padding: '0px 1.0875rem 1.45rem',
             paddingTop: 0,
           }}
